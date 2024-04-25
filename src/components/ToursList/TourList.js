e6846e1e3aa4a630182bd6261d5f0bdb0bc49cff
Joseph <joseph.meza@unmsm.edu.pe>
2024-04-25 11:41:59 -0500

    Tour List

---
+++ src/components/ToursList/TourList.js
@@ -0,0 +1,18 @@
+export const VIAJE_ESCOLAR = [
+  {
+    img: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/10/28/0a/04.jpg',
+    title: 'Paracas',
+    description: 'Tour Ecològico a la Reserva Nacional, dentro de la Peninsula en la Bahìa de Paracas. Tour con reconocimiento de avifauna marina. Historia milenaria cultura Paracas asombrosos ciruganos e increìbles tejedores que vivieron en este desierto hace 2000 años. Dentro de la Reserva observaremos el famoso paisaje lunar y los naturales cortes terrestres.',
+    price: 'Desde S/99',
+    duration: 'Full Day de 12 a 15 horas',
+    aditionalServices: 'Tour Maritimo S/35',
+  },
+  {
+    img: 'https://cdn.cuponidad.pe/images/Deals/lunahuana07eco.jpg',
+    title: 'Lunahuana',
+    description: 'Verde Valle de la sierra de Lima, circuito ecològico por Cañete, Cerro Azul. Pueblo de piratas, Castillo Unanue, Ciudadela Inka de Inkawasi, puente colgante de Catapalla. Zona agrìcola, visita a vitivinicolas artesanales y a la zona apicola. Lunahuana goza de un clima preferente de sol 10 meses al año',
+    price: 'Desde S/89',
+    duration: 'Full Day de 09 a 12 horas',
+    aditionalServices: 'Almuerzo Campestre S/20',
+  }
+]
